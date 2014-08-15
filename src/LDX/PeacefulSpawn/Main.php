e8a7028f595656b30d8d8b52641c7dd993cdc64a
Luke Darling <HTMLguy7@gmail.com>
2014-08-15 11:41:43 -0400

    Update Main.php

--- src/LDX/PeacefulSpawn/Main.php
+++ src/LDX/PeacefulSpawn/Main.php
@@ -10,24 +10,29 @@ use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\entity\EntityDamageEvent;
 class Main extends PluginBase implements Listener {
-  public function onLoad() {
-    $this->getLogger()->info(TextFormat::YELLOW . "Loading PeacefulSpawn v2.0 by LDX...");
-  }
   public function onEnable() {
     $this->enabled = true;
-    $this->getLogger()->info(TextFormat::YELLOW . "Enabling PeacefulSpawn...");
     $this->getServer()->getPluginManager()->registerEvents($this,$this);
-    
+    if(!file_exists($this->getDataFolder() . "config.yml")) {
+      @mkdir($this->getDataFolder());
+      file_put_contents($this->getDataFolder() . "config.yml",$this->getResource("config.yml"));
+    }
+    $c = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
+    $this->r = $c["Radius"];
   }
   public function onCommand(CommandSender $issuer,Command $cmd,$label,array $args) {
-    if(strtolower($cmd->getName()) == "ps") {
-      $this->enabled = !$this->enabled;
-      if($this->enabled) {
-        $issuer->sendMessage("[PeacefulSpawn] Spawn protection enabled!");
-        $this->getLogger()->info(TextFormat::YELLOW . "Spawn protection enabled!");
+    if(strtolower($cmd->getName()) == "ps" ) {
+      if($issuer->hasPermission("peacefulspawn") || $issuer->hasPermission("peacefulspawn.toggle")) {
+        $this->enabled = !$this->enabled;
+        if($this->enabled) {
+          $issuer->sendMessage("[PeacefulSpawn] Spawn protection enabled!");
+          $this->getLogger()->info(TextFormat::YELLOW . "Spawn protection enabled!");
+        } else {
+          $issuer->sendMessage("[PeacefulSpawn] Spawn protection disabled!");
+          $this->getLogger()->info(TextFormat::YELLOW . "Spawn protection disabled!");
+        }
       } else {
-        $issuer->sendMessage("[PeacefulSpawn] Spawn protection disabled!");
-        $this->getLogger()->info(TextFormat::YELLOW . "Spawn protection disabled!");
+        $issuer->sendMessage("You do not have permission to toggle spawn protection.");
       }
       return true;
     } else {
@@ -38,18 +43,14 @@ class Main extends PluginBase implements Listener {
   * @param EntityDamageEvent $event
   *
   * @priority HIGHEST
-  * @ignoreCancelled false
+  * @ignoreCancelled true
   */
   public function onHurt(EntityDamageEvent $event) {
     $entity = $event->getEntity();
-    $v = new Vector3($entity->getLevel()->getSpawn()->x,$entity->y,$entity->getLevel()->getSpawn()->z);
-    $r = $this->getServer()->getSpawnRadius();
-    if($entity instanceof Player && $entity->distance($v) <= $r && $this->enabled == true) {
+    $v = new Vector3($entity->getLevel()->getSpawnLocation()->getX(),$entity->getPosition()->getY(),$entity->getLevel()->getSpawnLocation()->getZ());
+    if(($entity instanceof Player) && ($entity->getPosition()->distance($v) <= $this->r) && ($this->enabled == true)) {
       $event->setCancelled();
     }
-  }
-  public function onDisable() {
-    $this->getLogger()->info(TextFormat::YELLOW . "Disabling PeacefulSpawn...");
   }
 }
 ?>
