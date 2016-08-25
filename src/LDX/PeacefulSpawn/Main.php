a65e726ea7f6fdbb762ea3bda35eef3873890f86
Cøsʍic Sαѵαɢe <jross3740@gmail.com>
2016-08-24 20:35:14 -0400

    Update Main.php

--- src/LDX/PeacefulSpawn/Main.php
+++ src/LDX/PeacefulSpawn/Main.php
@@ -8,23 +8,22 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
+
 class Main extends PluginBase implements Listener {
   public function onEnable() {
     $this->enabled = true;
-    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-  public function onCommand(CommandSender $issuer,Command $cmd,$label,array $args) {
+  public function onCommand(CommandSender $issuer, Command $cmd, $label, array $args) {
     if(strtolower($cmd->getName()) == "ps" ) {
       if($issuer->hasPermission("peacefulspawn") || $issuer->hasPermission("peacefulspawn.toggle")) {
         $this->enabled = !$this->enabled;
         if($this->enabled) {
-          $issuer->sendMessage("[PeacefulSpawn] Spawn protection enabled!");
-          $this->getLogger()->info(TextFormat::YELLOW . "Spawn protection enabled!");
+          $issuer->sendMessage("Spawn security enabled!");
+          $this->getLogger()->info(TextFormat::YELLOW . "Spawn security enabled!");
         } else {
-          $issuer->sendMessage("[PeacefulSpawn] Spawn protection disabled!");
-          $this->getLogger()->info(TextFormat::YELLOW . "Spawn protection disabled!");
+          $issuer->sendMessage("Spawn security disabled!");
+          $this->getLogger()->info(TextFormat::YELLOW . "Spawn security disabled!");
         }
       } else {
         $issuer->sendMessage("You do not have permission to toggle spawn protection.");
@@ -40,12 +39,21 @@ class Main extends PluginBase implements Listener {
   * @priority HIGHEST
   * @ignoreCancelled true
   */
-  public function onHurt(EntityDamageEvent $event) {
+  public function onHurt(\pocketmine\event\entity\EntityDamageEvent $event) {
     $entity = $event->getEntity();
     $v = new Vector3($entity->getLevel()->getSpawnLocation()->getX(),$entity->getPosition()->getY(),$entity->getLevel()->getSpawnLocation()->getZ());
     $r = $this->getServer()->getSpawnRadius();
-    if(($entity instanceof Player) && ($entity->getPosition()->distance($v) <= $r) && ($this->enabled == true)) {
-      $event->setCancelled();
+    if(($entity instanceof \pocketmine\Player) && ($entity->getPosition()->distance($v) <= $r) && ($this->enabled == true)) {
+      $event->setCancelled(true);
+    }
+  }
+  
+  public function onPrime(\pocketmine\event\entity\ExplosionPrimeEvent $event) {
+    $entity = $event->getEntity();
+    $v = new Vector3($entity->getLevel()->getSpawnLocation()->getX(),$entity->getPosition()->getY(),$entity->getLevel()->getSpawnLocation()->getZ());
+    $r = $this->getServer()->getSpawnRadius();
+    if(($entity instanceof \pocketmine\entity\PrimedTNT) && ($entity->getPosition()->distance($v) <= $r) && ($this->enabled == true)) {
+      $event->setCancelled(true);
     }
   }
 }
